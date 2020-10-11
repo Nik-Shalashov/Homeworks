fc076c7d2c99690d7e68ca2c30cb9a49ff860785
Nik-Shalashov <monitor1@inbox.ru>
2020-10-11 19:51:35 +0700

    Initial commit

---
+++ src/lesson1/Main.java
@@ -0,0 +1,81 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class Main {
+    //Задание 1
+    public static void main(String[] args) {
+
+        //Задание 2
+        byte bit8 = 56;
+        short sh = 24789;
+        int a = 500;
+        long lg = -894546548L;
+        float fl = 89.99f;
+        double db = -56.245;
+        char s = 'G';
+        boolean bl = true;
+
+        System.out.println(calculate(8, 6, 6, 0));
+        System.out.println(compareSum(80, 9));
+        compareToZero(-6);
+        System.out.println(checkBelowZero(5));
+        sayHello("Никита");
+        whatsYear(900);
+
+    }
+
+    //Задание 3
+    static int calculate(int a, int b, int c, int d) {
+        if (d == 0) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Параметр d не может быть равным 0, измените его");
+            d = in.nextInt();
+            in.close();
+        }
+        return a * (b + (c / d));
+    }
+
+    //Задание 4
+    static boolean compareSum(int a, int b) {
+        int c = a + b;
+        if (c >= 10 && c <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Задание 5
+    static void compareToZero(int a) {
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    //Задание 6
+    static boolean checkBelowZero(int a) {
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Задание 7
+    static void sayHello(String name) {
+        System.out.printf("Привет, " + name + "!\n");
+    }
+
+    //Задание 8*
+    static void whatsYear(int year) {
+        if ((year % 4) != 0 || (year % 400) != 0 && (year % 100) == 0) {
+            System.out.println("Год  не високосный");
+        } else {
+            System.out.println("Год високосный");
+        }
+    }
+
+}
