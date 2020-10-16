34e30373947b2657be9269f1d18d6453ac402607
Nik-Shalashov <monitor1@inbox.ru>
2020-10-16 20:14:51 +0700

    Выполненная доработанная домашняя работа №3 задание №1 исправленное

--- src/lesson3/Task1.java
+++ src/lesson3/Task1.java
@@ -29,20 +29,21 @@ public class Task1 {
         int randNum = random.nextInt(10);
         for (int i = 0; i < 3; i++) {
             userNum = scanner.nextInt();
-            if (i == 2) {
+            if (userNum == randNum) {
+                System.out.println("Поздравляю! Вы угадали число!");
+                endGame();
+            } else if (i == 2) {
                 System.out.println("К сожалению, вы не угадали число.");
                 endGame();
             } else if (userNum < randNum) {
                 System.out.println("Ваше число меньше загаданного. Введите другое число:");
             } else if (userNum > randNum) {
                 System.out.println("Ваше число больше загаданного. Введите другое число:");
-            } else if (userNum == randNum) {
-                System.out.println("Поздравляю! Вы угадали число!");
-                endGame();
             }
-
         }
 
     }
 
 }
+
+
